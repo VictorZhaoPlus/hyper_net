1475123290c892ed8c2867059dfe96a4f65e1262
Qian Liu <ooeyusea@gmail.com>
2017-03-25 14:54:51 +0800

    1.create.hs changed

--- src/logic/create.hs
+++ src/logic/create.hs
@@ -7,24 +7,26 @@ main = do
 	name <- getLine
 	putStrLn "please input author name:"
 	author <- getLine
+	putStrLn "please input module name:"
+	md <- getLine
 	t <- T.getCurrentTime
-	D.createDirectoryIfMissing True (map C.toLower name)
-	copyTemp name author (time t) "template/CMakeLists.txt" ((map C.toLower name) ++ "/CMakeLists.txt")
-	copyTemp name author (time t) "template/Template.h" ((map C.toLower name) ++ "/" ++ (toH name))
-	copyTemp name author (time t) "template/Template.cpp" ((map C.toLower name) ++ "/" ++ (toCPP name))
-	copyTemp name author (time t) "template/main.cpp" ((map C.toLower name) ++ "/main.cpp")
-	copyTemp (toInterface name) author (time t) "template/Interface.h" ("interface/" ++ (toH $ toInterface name))
+	D.createDirectoryIfMissing True (md ++ "/" ++ (map C.toLower name))
+	copyTemp name author md (time t) "template/CMakeLists.txt" (md ++ "/" ++ (map C.toLower name) ++ "/CMakeLists.txt")
+	copyTemp name author md (time t) "template/Template.h" (md ++ "/" ++ (map C.toLower name) ++ "/" ++ (toH name))
+	copyTemp name author md (time t) "template/Template.cpp" (md ++ "/" ++ (map C.toLower name) ++ "/" ++ (toCPP name))
+	copyTemp name author md (time t) "template/main.cpp" (md ++ "/" ++ (map C.toLower name) ++ "/main.cpp")
+	copyTemp (toInterface name) author md (time t) "template/Interface.h" ("interface/" ++ (toH $ toInterface name))
 	where
 		time = T.formatTime T.defaultTimeLocale "%B %d, %Y, %I:%M %p"
 		
-copyTemp :: String -> String -> String -> FilePath -> FilePath -> IO()
-copyTemp name author time src dst = do
+copyTemp :: String -> String -> String -> String -> FilePath -> FilePath -> IO()
+copyTemp name author md time src dst = do
 	handle <- I.openFile src I.ReadMode
 	contents <- I.hGetContents handle
 	writeFile dst (rep contents)
 	I.hClose handle
 	where
-		rep = (replace '@' (map C.toUpper name)).(replace '+' (map C.toLower name)).(replace '&' name).(replace '%' time).(replace '^' author)
+		rep = (replace '@' (map C.toUpper name)).(replace '+' (map C.toLower name)).(replace '&' name).(replace '%' time).(replace '^' author).(replace '-' md)
 	
 
 replace :: Char -> String -> String -> String
--- src/logic/system/main.cpp
+++
@@ -1,4 +0,0 @@
-#include "Cluster.h"
-
-GET_DLL_ENTRANCE;
-CREATE_MODULE(Cluster)
